8730e266d07d0c8711845302d7c548a6932caa29
Yaswitha Boppana <323103210032.yaswitha@gvpcew.ac.in>
2025-06-14 12:23:43 +0530

    Setup basic overlay

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <QApplication>
+#include <QWidget>
 #include <iostream>
 
-int main() {
-    std::cout << "CMake basic build working!" << std::endl;
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    std::cout << "App started!" << std::endl;
+
+    QWidget window;
+    window.setWindowTitle("Overlay Example");
+    window.setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool);
+    window.resize(400, 300);
+    window.show();
+
+    return app.exec();
 }
